0a1c41836afa342e3c708826c2942275d33cf527
Alex Cruz <alex@dirtyunicorns.com>
2018-11-07 12:28:39 -0600

    Clean up battery styles
    - Remove code for landscape battery. Won't be used any time soon.
    
    Change-Id: I95f15053121a0e820efcedb257096d298d75028d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7997,10 +7997,9 @@ public final class Settings {
         /**
          * Display style of the status bar battery information
          * 0: Display the battery an icon in portrait mode
-         * 1: Display the battery an icon in landscape mode
-         * 2: Display the battery as a circle
-         * 3: Display the battery as text
-         * 4: Do not display the battery
+         * 1: Display the battery as a circle
+         * 2: Display the battery as text
+         * 3: Do not display the battery
          * default: 0
          * @hide
          */
