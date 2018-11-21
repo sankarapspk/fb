9482919e1b6920f3eb556ffedb3edd43d88bb169
ezio84 <brabus84@gmail.com>
2018-11-21 22:31:39 +0100

    Battery icon: force show pct in QS header and when charging/powersave
    like aosp already wanted to do (QuickStatusBarHeader setForceShowPercent
    was being overridden by the tuner observer added by custom battery
    commits to BatteryMeterView).
    
    This is also good for users that don't want the percentage in the
    statusbar but still want to be able to check it in the QS panel
    
    Change-Id: Ibc87469f31b24b1ccb3235ef43c5f1f6b199af1d

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -71,6 +71,7 @@ public class BatteryMeterView extends LinearLayout implements
     private int mTextColor;
     private int mLevel;
     private boolean mForceShowPercent;
+    private boolean misQsbHeader;
     private boolean mCharging;
     private boolean mPowerSave;
 
@@ -283,11 +284,9 @@ public class BatteryMeterView extends LinearLayout implements
         final boolean showing = mBatteryPercentView != null;
         int style = Settings.System.getIntForUser(getContext().getContentResolver(),
                 SHOW_BATTERY_PERCENT, 1, mUser);
-        if (mForceShowPercent
-                || (getMeterStyle() != BatteryMeterDrawableBase.BATTERY_STYLE_HIDDEN
-                && (mForceShowPercent || mPowerSave || mCharging))) {
-            style = 1; // Default view
-        }
+
+        boolean showAnyway = mForceShowPercent || mPowerSave || mCharging;
+        if (showAnyway) style = 1; // Default view
         switch (style) {
             case 1:
                 if (!showing) {
@@ -409,12 +408,16 @@ public class BatteryMeterView extends LinearLayout implements
         }
     }
 
+    public void isQsbHeader() {
+        misQsbHeader = true;
+    }
+
     private void updateBatteryStyle(String styleStr) {
         final int style = styleStr == null ?
                 BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT : Integer.parseInt(styleStr);
         mDrawable.setMeterStyle(style);
 
-        mForceShowPercent = false;
+        mForceShowPercent = misQsbHeader ? true : false;
 
         switch (style) {
             case BatteryMeterDrawableBase.BATTERY_STYLE_TEXT:
